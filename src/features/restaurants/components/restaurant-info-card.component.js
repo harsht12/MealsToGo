7dc53adc2346aae641eca3d28308b221d021714d
hpat6151 <hpat6151@uni.sydney.edu.au>
2023-01-08 19:26:02 +1100

    commented out unused vars for now

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Avatar, Button, Card, Text } from "react-native-paper";
+import { Card, Text } from "react-native-paper";
 import { colors } from "../../../utils/colors";
 import { spacing } from "../../../utils/sizes";
 
 export const RestaurantInfoCard = ({ restuarant = {} }) => {
   const {
     name = "Some Restaurant",
-    icon,
+    // icon,
     photos = [
       "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/12/og-for-street-food-in-sydney.jpg",
     ],
-    address = "100 some random street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
+    // address = "100 some random street",
+    // isOpenNow = true,
+    // rating = 4,
+    // isClosedTemporarily,
   } = restuarant;
   return (
     <Card elevation={5} style={styles.card}>
