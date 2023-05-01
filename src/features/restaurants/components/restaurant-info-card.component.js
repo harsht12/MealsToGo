7a62933eea5cc32c58ba1bc4aefd6118e8ac9b35
hpat6151 <hpat6151@uni.sydney.edu.au>
2023-05-01 21:16:12 +1000

    added closed temporarily icon

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, Text } from "react-native-paper";
+import { Text, View, Image } from "react-native";
+import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
@@ -54,7 +55,7 @@ const RestuarantCardCover = styled(Card.Cover)`
 export const RestaurantInfoCard = ({ restuarant = {} }) => {
   const {
     name = "Some Restaurant",
-    // icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/12/og-for-street-food-in-sydney.jpg",
     ],
@@ -77,7 +78,15 @@ export const RestaurantInfoCard = ({ restuarant = {} }) => {
               <StyledSvgXml xml={star} width={20} height={20} />
             ))}
             <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <View style={{ paddingLeft: 16 }} />
               {isOpenNow && <StyledSvgXml xml={open} width={20} height={20} />}
+              <View style={{ paddingLeft: 16 }} />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
             </SectionEnd>
           </Rating>
         </Section>
