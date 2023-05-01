f18d139f66c52a210f899ba5bd13bdd99a3a1da6
hpat6151 <hpat6151@uni.sydney.edu.au>
2023-05-01 20:47:52 +1000

    displaying address now

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,10 +3,19 @@ import { Card, Text } from "react-native-paper";
 import styled from "styled-components/native";
 
 const Title = styled(Text)`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
   color: ${(props) => props.theme.colors.ui.primary};
   font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.title};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -26,14 +35,17 @@ export const RestaurantInfoCard = ({ restuarant = {} }) => {
       "https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/12/og-for-street-food-in-sydney.jpg",
     ],
     address = "100 some random street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily,
+    // isOpenNow = true,
+    // rating = 4,
+    // isClosedTemporarily,
   } = restuarant;
   return (
     <RestaurantCard elevation={5}>
       <RestuarantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
